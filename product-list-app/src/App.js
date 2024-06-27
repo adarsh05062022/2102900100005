c77db9ed28b87028a98be262813cad55c8d71f08
Adarsh Dubey <anmold652@gmail.com>
2024-06-27 16:02:40 +0530

    app setup

---
+++ product-list-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+import { ChakraProvider } from '@chakra-ui/react'
+
+function App() {
+  return (
+    <ChakraProvider>
+    
+  </ChakraProvider>
+  );
+}
+
+export default App;
