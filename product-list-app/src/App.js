5ce650c81b8db949a6c409b84bedb4ad461f17ab
Adarsh Dubey <anmold652@gmail.com>
2024-06-27 16:17:56 +0530

    route setup

--- product-list-app/src/App.js
+++ product-list-app/src/App.js
@@ -1,13 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 
 import { ChakraProvider } from '@chakra-ui/react'
 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import ProductsPage from './Pages/ProductsPage';
+import ProductDetailsPage from './Pages/ProductDetailsPage';
+
 function App() {
   return (
     <ChakraProvider>
-    
+       <Router>
+      <Routes>
+        <Route exact path="/" element={<ProductsPage/>} />
+        <Route exact path="/products/:productId" element={<ProductDetailsPage />} />
+      </Routes>
+    </Router>
   </ChakraProvider>
   );
 }
